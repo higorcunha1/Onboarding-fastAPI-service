f19adf5ddc36d048fd35f019c8f94fe82fb625d6
hdacunha <higor.dacunha@windriver.com>
2024-08-30 16:10:30 -0300

    [TR-001]changing-the-initcontainer

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from .routes.routes import router as main_router
-
-app = FastAPI()
-app.include_router(main_router) # including the router in the app
-
---
+++ main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+from app.routes.routes import router as main_router
+
+app = FastAPI()
+
+app.include_router(main_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="100.0.0.1", port=8000, reload=True) # running the app
+
