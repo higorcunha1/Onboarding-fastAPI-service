a7f4dad647a074bc788f754a5dd045fce3acd979
hdacunha <higor.dacunha@windriver.com>
2024-09-05 17:05:23 -0300

    [TR-003]adding-postgresql-database

--- app/controller/controller.py
+++ app/controller/controller.py
@@ -1,7 +1,30 @@
 from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+from app.models.models import User, Book, Booking
 
 def hello_response():
     return JSONResponse(content={"message": "Hello World"})
 
 def echo_response(value: str):
     return JSONResponse(content={"message": f"Received: {value}"})
+
+def create_user(db: Session, name: str):
+    db_user = User(name=name)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def create_book(db: Session, name: str, genre: str, copies_number: int):
+    db_book = Book(name=name, genre=genre, copies_number=copies_number)
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+def create_booking(db: Session, start_date: str, end_date: str, book_id: int, user_id: int):
+    db_booking = Booking(start_date=start_date, end_date=end_date, book_id=book_id, user_id=user_id)
+    db.add(db_booking)
+    db.commit()
+    db.refresh(db_booking)
+    return db_booking
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,8 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from app.controller.controller import hello_response, echo_response
+from sqlalchemy.orm import Session
+from app.controller.controller import create_user, create_book, create_booking
+from app.database import SessionLocal
 
 router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/users/")
+def add_user(name: str, db: Session = Depends(get_db)):
+    return create_user(db, name)
+
+@router.post("/books/")
+def add_book(name: str, genre: str, copies_number: int, db: Session = Depends(get_db)):
+    return create_book(db, name, genre, copies_number)
+
+@router.post("/bookings/")
+def add_booking(start_date: str, end_date: str, book_id: int, user_id: int, db: Session = Depends(get_db)):
+    return create_booking(db, start_date, end_date, book_id, user_id)
+
 
 @router.get("/hello") # hello world route (GET)
 async def hello_world():
